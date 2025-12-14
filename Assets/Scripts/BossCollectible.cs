593257058e0b189c65e7673ba1acc986b99ce5c7
Kristián Červenka <kristian.cervenka@gmail.com>
2025-12-14 22:57:41 +0100

    Boss phases added, added memory fragments content

--- Assets/Scripts/BossCollectible.cs
+++ Assets/Scripts/BossCollectible.cs
@@ -4,19 +4,24 @@ public class BossCollectible : MonoBehaviour
 {
     public int damageToBoss = 10;
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("2D Trigger hit by: " + other.name);
+
         if (other.CompareTag("Player"))
         {
+            Debug.Log("BossCollectible: Player collected the item.");
             Boss boss = FindObjectOfType<Boss>();
 
             if (boss != null)
             {
                 boss.TakeDamage(damageToBoss);
-                Debug.Log("BossCollectible: Player collected an item and damaged the boss by " + damageToBoss);
+                Debug.Log("BossCollectible: Damaged boss by " + damageToBoss);
             }
 
             Destroy(gameObject);
         }
     }
+
+
 }

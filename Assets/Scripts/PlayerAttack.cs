f1b1b886cf19f8dc1d6f28f2ef9fe355341d2a1b
Kristián Červenka <kristian.cervenka@gmail.com>
2025-12-19 00:28:50 +0100

    Updated all player animations set pivot to center

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -11,37 +11,63 @@ public class PlayerAttack : MonoBehaviour
     public float attackRange;
 
     public int damage;
+    public InventoryItem swordItem; // Drag your Sword item here in the Inspector
+
+
+    public Animator anim;
+
 
     void Update()
     {
         if (timeBetweenAttacks <= 0)
         {
-            if (Input.GetKey(KeyCode.F)) // F button
+            if (Input.GetKeyDown(KeyCode.F)) // Attack key
             {
-                Debug.Log("Attack!");
-                
-                // Attack regular enemies
-                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemyLayers);
-                foreach (Collider2D enemy in enemiesToDamage)
+                bool hasSword = InventoryManager.instance.HasItem(swordItem);
+
+
+                if (anim != null)
                 {
-                    enemy.GetComponent<Enemy>().TakeDamage(damage);
-                    Debug.Log("Damaged enemy: " + enemy.name);
+                    Debug.Log("Attempting to play attack: " + (hasSword ? "attackSword" : "attackUnarmed"));
+                    anim.SetTrigger(hasSword ? "attackSword" : "attackUnarmed");
                 }
-                // Attack boss
-                Collider2D[] bossesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, bossLayers);
-                foreach (Collider2D boss in bossesToDamage)
-                {
-                    boss.GetComponent<Boss>().TakeDamage(damage);
-                    Debug.Log("Damaged boss: " + boss.name);
-                }
+
+                Debug.Log("Attack Triggered!");
+                timeBetweenAttacks = startTimeBetweenAttacks;
             }
-            timeBetweenAttacks = startTimeBetweenAttacks;
         }
         else
         {
             timeBetweenAttacks -= Time.deltaTime;
         }
     }
+
+    public void DoAttackHit()
+    {
+        // Attack regular enemies
+        Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, enemyLayers);
+        foreach (Collider2D enemy in enemiesToDamage)
+        {
+            if (enemy.TryGetComponent(out Enemy e))
+            {
+                e.TakeDamage(damage);
+            }
+        }
+
+        // Attack boss
+        Collider2D[] bossesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, bossLayers);
+        foreach (Collider2D boss in bossesToDamage)
+        {
+            if (boss.TryGetComponent(out Boss b))
+            {
+                b.TakeDamage(damage);
+            }
+        }
+
+        Debug.Log("Attack Hit Applied!");
+    }
+
+
 
     void OnDrawGizmosSelected()
     {
